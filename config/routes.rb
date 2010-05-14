25a4a19700227b7ee1549458c92f2099e638a0d0
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2010-05-14 08:47:16 -0400

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
 	map.resources :users
-	map.contact '/contact',	:controller => 'pages', :action => 'contact'
-	map.about		'/about',		:controller => 'pages', :action => 'about'
-	map.help		'/help',		:controller => 'pages', :action => 'help'
-
-	map.signup	'/signup',	:controller => 'users', :action => 'new'
-	
-	map.root								:controller => 'pages', :action => 'home'
+	map.resources :sessions, :only => [:new, :create, :destroy]
+	map.signin  '/signin',  :controller => 'sessions',  :action => 'new'
+	map.signout '/signout', :controller => 'sessions',  :action => 'destroy'
+	map.contact '/contact',	:controller => 'pages',     :action => 'contact'
+	map.about		'/about',		:controller => 'pages',     :action => 'about'
+	map.help		'/help',		:controller => 'pages',     :action => 'help'
+	map.signup	'/signup',	:controller => 'users',     :action => 'new'
+	map.root								:controller => 'pages',     :action => 'home'
 	
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
